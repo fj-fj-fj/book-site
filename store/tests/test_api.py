daef05bd0b17b33e1aa65577ae801882e09ff5ab
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-12-11 04:33:44 +0300

    Set IsAuthenticatedIsAuthenticated, add CRUD tests

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,6 +11,7 @@ from store.serializers import BooksSerializer
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book1 = Book.objects.create(name='test1', price=25, author='author 1')
         self.book2 = Book.objects.create(name='test2', price=55, author='author 2')
         self.book3 = Book.objects.create(name='test3 author 2', price=25, author='author 3')
@@ -39,3 +43,43 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BooksSerializer([self.book2, self.book1, self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            'name': 'a bite or py',
+            'price': '200.2',
+            'author': 'au au'
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book1.id,))
+        data = {
+            'name': self.book1.name,
+            'price': 666,
+            'author': self.book1.author
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        # self.assertEqual(666, self.book1.price)
+        # AssertionError: 666 != 25  # wtf
+        # self.book1 = Book.objects.get(id=self.book1.id)  # or
+        self.book1.refresh_from_db()
+        self.assertEqual(666, self.book1.price)  # OK
+
+    def test_delete(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-detail', args=(self.book3.id,))
+        self.client.force_login(self.user)
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(2, Book.objects.all().count())
--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -6,19 +6,21 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book1 = Book.objects.create(name='test1', price=25)
-        book2 = Book.objects.create(name='test2', price=55)
+        book1 = Book.objects.create(name='test1', price=25, author='author 1')
+        book2 = Book.objects.create(name='test2', price=55, author='author 2')
         data = BooksSerializer([book1, book2], many=True).data
         expected_data = [
             {
                 'id': book1.id,
                 'name': 'test1',
-                'price': '25.00'
+                'price': '25.00',
+                'author': 'author 1'
             },
             {
                 'id': book2.id,
                 'name': 'test2',
-                'price': '55.00'
+                'price': '55.00',
+                'author': 'author 2'
             }
         ]
         self.assertEqual(expected_data, data)
